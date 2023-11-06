7baee514e31628ef3cf369dabeee0addd395d522
alanbrzu <alanbrzu@gmail.com>
2023-11-06 18:09:36 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Typescript Node + Express Template
+
+## Usage
+
+Create a `.env` file with the PORT variable
+
+Development: `yarn dev`
+
+Production: `yarn start`
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT ?? '3001'
 
 app.use(
     cors(),
